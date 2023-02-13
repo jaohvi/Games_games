782acc430af6c11dd2c053918811c0d4dbfa22cd
jaohvi <vicentelegal@hotmail.com>
2023-02-13 12:08:08 -0400

    alteracoes

--- forca.py
+++ forca.py
@@ -1,3 +1,21 @@
 
 
-def forca():
+def jogar():
+    print("Bem Vindo ao jogo da Forca")
+    secrect_word = "banana"
+    hit= False
+    hanged= False
+    while(not hanged and not hit):
+        index = 0
+        chute=input("Digite uma letra")
+        for letra in secrect_word:
+            index = index + 1 #ele tem que estar aqui, pois a cada for eu vejo a posição verificada
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+
+
+    print("Jogando....")
+
+
+if(__name__=="__main__"):
+    jogar()
