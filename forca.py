cb44f92619c48ae339a232e666787d5e783a6490
jaohvi <vicentelegal@hotmail.com>
2023-02-13 22:39:38 -0400

    alteracoes

--- forca.py
+++ forca.py
@@ -1,22 +1,22 @@
 
-
 def jogar():
     print("Bem Vindo ao jogo da Forca")
     secrect_word = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+    print(letras_acertadas)
     hit= False
     hanged= False
     while(not hanged and not hit):
-        index = 0
         chute = input("Digite uma letra")
         chute = chute.strip( )
+        index=0
         for letra in secrect_word:
-            index = index + 1 #ele tem que estar aqui, pois a cada for eu vejo a posição verificada
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
-
-
+                letras_acertadas[index]=letra
+            index = index + 1  # ele tem que estar aqui, pois a cada for eu vejo a posição verificada
+        print(letras_acertadas)
+        letras_faltando=str(letras_acertadas.count("_"))
+        print('Ainda faltam acertar {} letras'.format(letras_faltando))
     print("Jogando....")
-
-
-if(__name__=="__main__"):
+if(__name__ == "__main__"):
     jogar()
